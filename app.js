ea49cfb5348e5a19a6719faea5d376cbb1c588ba
Wei Qi <hiaweiqi@gmail.com>
2021-09-16 17:53:44 +0800

    solve heroku problem

--- app.js
+++ app.js
@@ -321,12 +321,12 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
 passport.use(User.createStrategy());
 
 passport.serializeUser(function (user, done) {
-    done(null, user.id);
+    return done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
-        done(err, user);
+        return done(err, user);
     });
 });
 
