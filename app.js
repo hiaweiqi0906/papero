9021fab3636f84a9fc8c916dc59c100d6da62eaf
Wei Qi <hiaweiqi@gmail.com>
2021-09-16 20:15:19 +0800

    solve heroku problem

--- app.js
+++ app.js
@@ -327,6 +327,7 @@ app.use(session({
   saveUninitialized: true,
   proxy: true, // add this line
   cookie: {
+    sameSite:'none',
     secure: true,
     maxAge: 1000 * 60 * 60 * 24,
     // store: new MongoStore({ mongoUrl: db })
